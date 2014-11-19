40276b0d26dd0fe72c77e7f37769e5d585f8ef8f
Donal <osulld13@tcd.ie>
2014-11-19 09:51:40 +0000

    Auto Commit 3 via commit.sh

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd TastierCompiler
-git add .
+git add --all
 git commit -m "Auto Commit 1 via commit.sh"
 cd ../TastierMachine
-git add .
+git add --all
 git commit -m "Auto Commit 2 via commit.sh"
 cd ..
-git add .
+git add --all
 git commit -m "Auto Commit 3 via commit.sh"
 git push origin master
 
